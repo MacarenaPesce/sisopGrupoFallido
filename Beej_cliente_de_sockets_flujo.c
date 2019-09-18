fb347a6cb0bd8e073ea2510b90b14610c29c1945
Leandro <micoluccileandro@gmail.com>
2019-09-18 18:23:29 -0300

    Client add

--- Beej_cliente_de_sockets_flujo.c
+++ Beej_cliente_de_sockets_flujo.c
@@ -19,29 +19,33 @@
 
     int main()
     {
-        int sockfd, numbytes;  
-        char buf[MAXDATASIZE];
-        //struct hostent *he;
-        struct sockaddr_in their_addr; // información de la dirección de destino 
+    	int numbytes;
+    	int socket_cliente;
+    	char buf[MAXDATASIZE];
+		struct sockaddr_in server_addr; // información de la dirección de destino
 
 
+    	memset(&(server_addr.sin_zero), '\0', 8);  // poner a cero el resto de la estructura
+    	server_addr.sin_family = AF_INET;    // Ordenación de bytes de la máquina
+    	server_addr.sin_port = htons(PORT);  // short, Ordenación de bytes de la red
+    	server_addr.sin_addr.s_addr = INADDR_ANY;//*((struct in_addr *)he->h_addr);
 
-        if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+
+        socket_cliente = socket(AF_INET, SOCK_STREAM, 0)
+
+
+        if (socket_cliente == -1) {
             perror("socket");
             exit(1);
         }
 
-        their_addr.sin_family = AF_INET;    // Ordenación de bytes de la máquina 
-        their_addr.sin_port = htons(PORT);  // short, Ordenación de bytes de la red 
-        their_addr.sin_addr.s_addr = INADDR_ANY;//*((struct in_addr *)he->h_addr);
-        memset(&(their_addr.sin_zero), '\0', 8);  // poner a cero el resto de la estructura
 
-        if (connect(sockfd, (struct sockaddr *)&their_addr,sizeof(struct sockaddr)) == -1) {
+        if (connect(socket_cliente, (struct sockaddr *)&server_addr,sizeof(struct sockaddr)) == -1) {
             perror("connect");
             exit(1);
         }
 
-        if ((numbytes=recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+        if ((numbytes=recv(socket_cliente, buf, MAXDATASIZE-1, 0)) == -1) {
             perror("recv");
             exit(1);
         }
@@ -50,7 +54,7 @@
 
         printf("Received: %s",buf);
 
-        close(sockfd);
+        close(socket_cliente);
 
         return 0;
     } 
