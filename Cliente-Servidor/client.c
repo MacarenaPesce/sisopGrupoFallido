768c0fd95ecf08782f27b21297a12b5feac65cd7
franchupedu <franchupedu@outlook.com>
2019-09-23 16:46:41 -0300

    Cambios al cliente

--- Cliente-Servidor/client.c
+++ Cliente-Servidor/client.c
@@ -1,9 +1,22 @@
 #include"client.h"
 
 #define IP "127.0.0.1"
-#define PUERTO "6667"
+#define PUERTO "1024"
 
-int connectToServer(char *IP, char *PORT, int client_socket) {
+int connectToServer(char *ip, char *PORT) {
+
+	struct addrinfo hints;
+	struct addrinfo *server_info;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;       // Permite que la maquina se encargue de verificar si usamos IPv4 o IPv6
+	hints.ai_socktype = SOCK_STREAM;   // Indica que usaremos el protocolo TCP
+	hints.ai_flags = AI_PASSIVE;
+
+	getaddrinfo(ip, PORT, &hints, &server_info);     // Carga en serverInfo los datos de la conexion
+
+	//Obtiene un socket (un file descriptor -todo en linux es un archivo-), utilizando la estructura serverInfo que generamos antes.
+	int client_socket = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
 
 	if(connect(client_socket, server_info->ai_addr, server_info->ai_addrlen) == -1){
 //		printf("Error al conectarse al servidor. Try again later.\n");
@@ -29,6 +42,7 @@ void sendMessages(int serverSocket) {
 	while(enviar){
 		printf(">");
 		fgets(message, PACKAGESIZE, stdin); // Lee una linea en el stdin (lo que escribimos en la consola) hasta encontrar un \n (y lo incluye) o llegar a PACKAGESIZE.
+		sprintf (message, "%c%s", strlen(message), message);
 		if(strcasecmp("exit\n", message) == 0) enviar = 0;  // Chequeo que el usuario no quiera salir
 		if (enviar) send(serverSocket, message, strlen(message) + 1, 0);  // Solo envio si el usuario no quiere salir.
 		
@@ -46,22 +60,8 @@ void closeConnection(int serverSocket) {
 
 int main(){
 	
-	struct addrinfo hints;
-	struct addrinfo *server_info;
-
-	memset(&hints, 0, sizeof(hints)); 
-	hints.ai_family = AF_UNSPEC;       // Permite que la maquina se encargue de verificar si usamos IPv4 o IPv6
-	hints.ai_socktype = SOCK_STREAM;   // Indica que usaremos el protocolo TCP
-	hints.ai_flags = AI_PASSIVE;
-
-	getaddrinfo(IP, PORT, &hints, &server_info);     // Carga en serverInfo los datos de la conexion
-
-
-	//Obtiene un socket (un file descriptor -todo en linux es un archivo-), utilizando la estructura serverInfo que generamos antes.
-	int client_socket = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
-
 	//Me conecto	
-	connectToServer(IP,PORT, client_socket);
+	int client_socket= connectToServer(IP,PUERTO);
 
 	//creamos el paquete y enviamos mensajes
 	sendMessages(client_socket);
