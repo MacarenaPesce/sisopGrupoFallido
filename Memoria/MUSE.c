122616fe8158158515065d3d0d6baef6a787abe0
Micolucci <micoluccileandro@gmail.com>
2019-10-11 11:24:09 -0300

    Fork de libmuse + pseudocódigo
    Se hizo un fork de la carpeta de la cátedra: libmuse y agregue pseudocódigo de como encarar MUSE

---
+++ Memoria/MUSE.c
@@ -0,0 +1,40 @@
+/*
+ *		2/10/2019	
+ * 		Módulo de MUSE 
+*/
+
+int main(){
+	
+	//int UPCM = malloc(1000) //única porción central de memoria
+	
+	int LISTEN_PORT = SaleDeConfig
+	int MEMORY_SIZE = SaleDeConfig
+	int PAGE_SIZE = SaleDeConfig
+	int SWAP_SIZE = SaleDeConfig 
+	
+	
+	void* asignameRAM(){
+		
+		buscoSegmentosQNoEstenLLenos()
+		
+		if(puedo llenar el segmento que todavía no está lleno)
+			llenarlo
+		else if(puedo agrandar ese segmento) 
+			agrandarlo
+		else if(puedo pedir un nuevo segmento)
+			pedirlo 
+		else
+			tengo llena la memoria 
+		log()
+	}
+		
+	void monitoriarProceso(){
+		int memoriaTPedida;
+		int memoriaTLiberada;
+		int memoryLeaksT;
+
+		if(termino) log()
+	}		
+	
+	
+}	
